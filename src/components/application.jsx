72e680ba9bc7e5f25175f11ba583f278a4e0f205
Mike Jackson <michael.lenard.jackson@gmail.com>
2025-05-04 18:39:28 -0400

    Merge fix

--- src/components/application.jsx
+++ src/components/application.jsx
@@ -3,11 +3,7 @@ import Game from './game';
 
 const Application = () => {
   return (
-<<<<<<< HEAD
     <main className="mx-auto my-8 flex w-96 flex-col gap-8">
-=======
-    <main className="flex flex-col gap-8 mx-auto my-8 w-96">
->>>>>>> pulling-content-up-exercise
       <Game />
       <ExpensiveComponent />
     </main>
